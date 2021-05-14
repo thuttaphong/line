e547ed3d22234ee26ea164855d56eba33d6711b0
thuttaphong <72042042+thuttaphong@users.noreply.github.com>
2021-05-14 16:36:30 +0700

    test

--- Tan.py
+++ Tan.py
@@ -4148,7 +4148,7 @@ def lineBot(op):
 while True:
     try:
         ops = oepoll.singleTrace(count=50)
-        print(ops)
+        print(ops.text)
         if ops is not None:
             for op in ops:
                 lineBot(op)
